cdebad07fd94ff3449224559cf5b061c27f66a96
Pelin Hangisi <pelinhangisi@gmail.com>
2022-04-29 08:52:09 +0300

    Update css file

--- energy/energy.js
+++ energy/energy.js
@@ -5,6 +5,7 @@ const text3 = document.getElementById('text3');
 // toplam nefes alma ve tutma alanı hesaplama 2.
 const totalTime3 = 6000;
 const breathTime3 = (totalTime3/6)*4;
+const breatOut = totalTime3 / 3;
 
 
 
